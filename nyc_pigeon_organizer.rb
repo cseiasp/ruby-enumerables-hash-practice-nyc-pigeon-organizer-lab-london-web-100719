654deb70195b9dea00c98b9adb9129d553cb014e
Caroline S-A <caroline.seilern@hotmail.com>
2019-10-01 13:15:19 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,6 @@
 def nyc_pigeon_organizer(data)
   
+<<<<<<< HEAD
    
   new_hash = {}
   
@@ -24,6 +25,23 @@ def nyc_pigeon_organizer(data)
   
   new_hash
   
+=======
+  new_hash = {}
+
+data.each do |attributes|
+  if data[attributes]
+  data[attributes].each do |descriptor|
+    data[attributes][descriptor].each do |pidgeon|
+      if !new_hash[pidgeon]
+      new_hash[pidgeon] = {}
+      end
+    end
+  end
+    
+  end
+  end
+data
+>>>>>>> 3b90892fe0e80aa5e76d1b0ce3d1570f51a97b3e
 end
 
 
